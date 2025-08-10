1dbe28e987cc58ae829332969f6a4ce240ea46b0
maks_bezr <maks.bezrodnyi@gmail.com>
2025-08-10 16:43:49 +0300

    Dev (#18)
    * empty
    
    * Fix4 (#7)
    
    * Dev (#4)
    
    * Initial commit from Create Next App
    
    * init commit
    
    * Initial commit
    
    * added db
    
    * fetching data
    
    * empty
    
    * empty (#3)
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * empty
    
    * added ppr
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * Fix5 (#9)
    
    * Dev (#4)
    
    * Initial commit from Create Next App
    
    * init commit
    
    * Initial commit
    
    * added db
    
    * fetching data
    
    * empty
    
    * empty (#3)
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * empty
    
    * search params + pagination
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * Fix5 (#11)
    
    * Dev (#4)
    
    * Initial commit from Create Next App
    
    * init commit
    
    * Initial commit
    
    * added db
    
    * fetching data
    
    * empty
    
    * empty (#3)
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * empty
    
    * search params + pagination
    
    * added action state
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * edit action status hook (#13)
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * added auth (#15)
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    * added metadata (#17)
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>
    
    ---------
    
    Co-authored-by: Maksym Bezrodnyi <maksym_bezrodnyi@epam.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,33 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Course Dashboard, built with App Router.',
+    url: 'https://next-learn-dashboard.vercel.sh',
+    siteName: 'Next.js Course Dashboard',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
 
 export default function RootLayout({
   children,
